b6edc25b4287e06aed99f64de64ec4460461846a
Yami Odymel <yamiodymel@gmail.com>
2016-02-06 10:42:29 +0000

    Rewrite the whole davai.

--- src/davai.php
+++ src/davai.php
@@ -9,118 +9,121 @@ class Davai
                        
     private $url = '';
     
-    public $rules = ['*' => '(.*)',
+    public $rules = ['*' => '(.+?)',
                      'i' => '([0-9]++)',
                      'a' => '([0-9A-Za-z]++)',
                      'h' => '([0-9A-Fa-f]++)'];
     
-    private $tempVariables = [];
+    
+    private $variables = [];
+    
+    private $parsedUrl   = [];
+    private $parsedPath  = [];
+    private $parsedGroup = [];
     
     function __construct()
     {
         $this->url = $_SERVER['REQUEST_URI'];
     }
     
-    function map($method, $url, $func, $name = null)
+    
+    
+    function map($method, $path, $func, $name = null)
     {
-        $this->parseUrl($url);
+        /** Separate the path by the slash */
+        $path             = explode('/', $path);
+        $this->parsedPath = array_filter(array_map('trim', $path));
+        
+        /** Separate the current url by the slash */
+        $url              = explode('/', $this->url);
+        $this->parsedUrl  = array_filter(array_map('trim', $url));
+        
+        
+        $this->groupUrl();
+        $this->validateRules();
+        $this->analyzeVariables();
+        
+        
+        e($this->parsedPath);
     }
     
-    function validatePartial($partial, $rule)
+    
+    
+    function validateRules()
     {
-        $regEx = $this->rules[$rule];
-        $regEx = '/^' . $regEx . '$/';
-    
-        preg_match($regEx, $partial, $matched);
-       
-        return $partial == $matched[0];
-    }
-    
-    
-    function separatePartial($rulePartial)
-    {
-        if(strpos($rulePartial, ':') === false)
-            return false;
+        foreach($this->parsedGroup as $singleGroup)
+        {
+            extract($singleGroup);
             
-        $rulePartial = explode(':', $rulePartial);
-
+            if($isPure && $rule == $content)
+                continue;
+        }
+        
+        e($this->parsedGroup);
+    }
     
-        $rule        = ltrim($rulePartial[0], '{');
-        $variable    = rtrim($rulePartial[1], '}');
+    function getRule($ruleName)
+    {
+        $isSplit = strpos($rulePartial, '|') !== false;
+        
+        if($isSplit)
+        {
+            return '^(' + $isSplit ')$';
+        }
+        else
+        {
+            
+        }
+    }
+    
+    
+    
+    function analyzeVariables()
+    {
+        foreach($this->parsedGroup as $singleGroup)
+        {
+            $variableName = $singleGroup['variable'];
+            $content      = $singleGroup['content'];
+            
+            
+            if($variableName)
+                $this->variables[$variableName] = $content;
+        }
+    }
+    
+    
+    function groupUrl()
+    {
+        foreach($this->parsedPath as $index => $singlePath)
+        {
+            $matchedUrl = isset($this->parsedUrl[$index]) ? $this->parsedUrl[$index] : false;
+            $partial  = $this->separatePartial($singlePath, $matchedUrl);
+        
+                
+                
+            $this->parsedGroup[] = $partial;
+        }
+    }
+    
+    
+    function separatePartial($partial, $matchedContent)
+    {
+        $isLazy      = substr($partial, -2, 1) === '?';
+        $isTag       = substr($partial,  0, 1) === '[';
+        
+        //if(!$isTag)
+        //    return false;
+        
+        $parsedPartial = explode(':', $partial);
+        $rule          = ltrim($parsedPartial[0], '[');
+        $variable      = rtrim($parsedPartial[1], $isLazy ? '?]' : ']');
+        
         
         return ['rule'     => $rule,
-                'variable' => $variable];
-    }
-    
-    
-    function parseUrl($url)
-    {
-        $url = explode('/', $url);
-        $url = array_filter(array_map('trim', $url));
-        
-        $currentUrl = explode('/', $this->url);
-        $currentUrl = array_filter(array_map('trim', $currentUrl));
-        
-        $collection = [];
-    
-        foreach($currentUrl as $key => $singleUrl)
-        {
-            if(!isset($url[$key]))
-                return false;
-                
-            $collection[] = [$singleUrl, $url[$key]];
-        }
-        
-        $this->tempVariables = [];
-        
-        foreach($collection as $singleCollection)
-        {
-            
-            $content = $singleCollection[0];
-            $rule    = $singleCollection[1];
-
-            if($content == $rule)
-                continue;
-            
-            $partial = $this->separatePartial($rule);
-            
-            if(!$partial)
-                if(!$this->validatePartial($content, $rule))
-                    return false;
-                
-            if($this->validatePartial($content, $partial['rule']))
-                $this->tempVariables[$partial['variable']] = $content;
-            else
-                return false;
-        }
-        
-        e($this->tempVariables);
-    }
-    
-    
-    function get()
-    {
-        
-    }
-    
-    function post()
-    {
-        
-    }
-    
-    function patch()
-    {
-        
-    }
-    
-    function delete()
-    {
-        
-    }
-    
-    function put()
-    {
-        
+                'variable' => $variable !== '' ? $variable : false,
+                'isLazy'   => $isLazy,
+                'isPure'   => !$isTag,
+                'content'  => $matchedContent];
     }
 }
 ?>

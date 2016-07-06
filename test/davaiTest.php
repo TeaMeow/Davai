2d0f40418d2384f9ce28ca5b18d1fd105ebc763e
Yami Odymel <yamiodymel@gmail.com>
2016-07-06 07:55:44 +0000

    Typo fixed.

--- test/davaiTest.php
+++ test/davaiTest.php
@@ -5,16 +5,16 @@ class DavaiTest extends \PHPUnit_Framework_TestCase
 {
     function testRoute()
     {
-        $davai     = new Davai();
-        $dava->url = 'http://www.example.com/test/user';
-        $isSuccess = false;
+        $Davai      = new Davai();
+        $Davai->url = 'http://www.example.com/test/user';
+        $isSuccess  = false;
 
         function a()
         {
             $isSuccess = true;
         }
 
-        $davai->get('/test/user/[i:userId]/[i:page?]', 'a');
+        $Davai->get('/test/user/[i:userId]/[i:page?]', 'a');
 
         $this->assertEquals(true, $isSuccess);
     }

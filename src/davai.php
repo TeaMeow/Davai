189980ecd4c5b4b42ca00abc0b83e39bc682771c
Yami Odymel <yamiodymel@gmail.com>
2016-07-06 13:38:40 +0000

    :(

--- src/davai.php
+++ src/davai.php
@@ -206,7 +206,7 @@ class Davai
         /** Separate the path by the slash */
         $path             = explode('/', $path);
         $this->parsedPath = array_filter(array_map('trim', $path));
-        echo . "--" . $this->url . "--\n";
+        echo "--" . $this->url . "--\n";
         /** Separate the current url by the slash */
         $url              = explode('/', strtok($this->url, '?'));
         $this->parsedUrl  = array_filter(array_map('trim', $url));
